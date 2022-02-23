2461dabfaa529b0b0c8b8d715e2bd4ef9d95d0b7
Johan Frank <johan@pinefox.se>
2022-02-23 16:34:01 +0100

    Character position validated

--- src/Personnummer.php
+++ src/Personnummer.php
@@ -242,6 +242,16 @@ final class Personnummer implements PersonnummerInterface
         foreach ($letters as $letter) {
             $positionFound = strpos($ssn, $letter);
 
+            // If this is 10 or 11 digits long, the reserve number character should be in 7th position:
+            if (is_numeric($positionFound) && strlen($ssn) < 12 && $positionFound !== 7) {
+                throw new PersonnummerException();
+            }
+
+            // If this is more than 12 digits long, the reserve number character should be in 9th position:
+            if (is_numeric($positionFound) && strlen($ssn) > 12 && $positionFound !== 9) {
+                throw new PersonnummerException();
+            }
+
             if ($positionFound !== false) {
                 return $positionFound;
             }
